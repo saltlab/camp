69960e529e4fea6676811727455580322d9b1c7a
Aakanksha Saha <aakankshasaha7393@gmail.com>
2014-07-16 14:39:02 -0700

    remove hidden files

---
+++ data/Testcases/Sikuli/iOs/MTGj/mtest1.sikuli/mtest1.sikuli/test1.py
@@ -0,0 +1,17 @@
+if exists("1404940822780.png"):
+    click("1404940853523.png")
+    wait(5)
+    find("DecklistCoun.png")
+    exists("DecklistCoun-1.png")
+    click("DecklistCoun-2.png")
+    wait(5)
+    
+else:
+    find("LayersCastin.png")   
+    exists("LayersCastin-1.png")
+    click("LayersCastin-2.png")
+    wait(5)
+    find("1404941060885.png")
+    exists("1404941094770.png")
+    click("1404941104143.png")
+    
