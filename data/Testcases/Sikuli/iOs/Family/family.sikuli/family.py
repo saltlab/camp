833bd308b85c7560ae0cf6dc36dadf76857a48d2
Aakanksha Saha <aakankshasaha7393@gmail.com>
2014-07-22 15:50:26 -0700

    no hidden files

---
+++ data/Testcases/Sikuli/iOs/Family/family.sikuli/family.py
@@ -0,0 +1,32 @@
+find("EIKE.png")
+exists("EIKE-1.png")
+if exists("0asm.png"):
+    find("IiE.png")
+    click("IiE-1.png")
+    wait(1)
+    find("EH.png")
+    exists("EH-1.png")
+    click("EH-2.png")
+    wait(1)
+    find("1406065170382.png")
+    click("1406065182701.png")
+    find("1406064870963.png")
+    click("1406064883661.png")
+    wait(1)
+    click("EH-3.png")
+    wait(1)
+    exists("FFH.png")
+    type("5.png","hello")
+    click("1406065405118.png")
+    wait(1)
+    find("1406065141778.png")
+    click("1406065150942.png")
+    wait(1)
+    find("1406065170382.png")
+    click("1406065182701.png")
+    wait(1)
+     
+    
+    
+    
+    
